7b49e9e89559a140ef6bf9cfc56853b26c3af04a
Camilo Rueda <camiloerueda@gmail.com>
2026-08-14 17:35:28 -0500

    agregaron los roles en user.model.js

--- backend/src/models/users.model.js
+++ backend/src/models/users.model.js
@@ -1,35 +1,35 @@
-import mongoose from "mongoose"
+import mongoose from "mongoose";
 
-const {Schema,model}=mongoose;
+const { Schema, model } = mongoose;
 
-const userSchema = new Schema({
-
-    nombreCompleto:{
-        type:String,
-        required:true
+const userSchema = new Schema(
+  {
+    nombreCompleto: {
+      type: String,
+      required: true,
     },
 
-    correo:{
-        type:String,
-        required:true,
-        unique:true
+    correo: {
+      type: String,
+      required: true,
+      unique: true,
     },
 
-    password:{
-        type:String,
-        required:true
+    password: {
+      type: String,
+      required: true,
     },
 
-    rol:{
-        
-        type: mongoose.Schema.Types.ObjectId,
-        ref:"Rol",
-        required:true
-        
-    }
+    rol: {
+      type: String,
+      enum: ["Administrador", "Cliente", "Empleado"],
+      default: "Cliente",
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
 
-},{
-    timestamps:true
-})
-
-export const userModel1=mongoose.model("Usuario",userSchema)
+export const userModel1 = mongoose.model("Usuario", userSchema);
