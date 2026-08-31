7dd256c23d686d4bbe430f0e5114ccb91c9bc00a
Camilo Rueda <camiloerueda@gmail.com>
2026-08-30 20:41:20 -0500

    feat: actualiza productos y componente product-card

---
+++ Frontend/src/app/components/product-card/product-card.ts
@@ -0,0 +1,9 @@
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+
+@Component({
+  selector: 'app-product-card',
+  imports: [],
+  templateUrl: './product-card.html',
+  styleUrl: './product-card.css',
+})
+export class ProductCard { }
--- Frontend/src/app/pages/products/products.ts
+++ Frontend/src/app/pages/products/products.ts
@@ -1,9 +1,15 @@
 import { Component } from '@angular/core';
 import { NavBar } from '../../components/nav-bar/nav-bar';
+import { ProductCard } from '../../components/product-card/product-card';
+
+
 @Component({
   selector: 'app-products',
   imports: [NavBar],
   templateUrl: './products.html',
   styleUrl: './products.css',
 })
-export class Products {}
+export class Products{ 
+}
+
+
