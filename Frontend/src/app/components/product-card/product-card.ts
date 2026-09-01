411835a79ee54c6c74421fcf0a126ad54463bd69
Camilo Rueda <camiloerueda@gmail.com>
2026-09-01 16:38:38 -0500

    Actualizar product card e interfaz de productos

--- Frontend/src/app/components/product-card/product-card.ts
+++ Frontend/src/app/components/product-card/product-card.ts
@@ -1,9 +1,17 @@
-import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { CurrencyPipe } from '@angular/common';
+
+import { Component, input } from '@angular/core';
+import { Products } from '../../interfaces/products';
 
 @Component({
   selector: 'app-product-card',
-  imports: [],
+  imports: [CurrencyPipe],
   templateUrl: './product-card.html',
   styleUrl: './product-card.css',
 })
-export class ProductCard { }
+export class ProductCard {
+  producto = input.required<Products>();
+  agregar(): void {
+    console.log('Producto agregado', this.producto());
+  }
+}
--- Frontend/src/app/interfaces/products.ts
+++ Frontend/src/app/interfaces/products.ts
@@ -1,1 +1,12 @@
-export interface Products {}
+export interface Products {
+  _id: string;
+  nombre: string;
+  descripcion?: string;
+  precio: number;
+  stock: number;
+  imagen?: string;
+  categoria: string;
+  estado: boolean;
+  cantidad: number;
+  disponible?: boolean;
+}
